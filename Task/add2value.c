029400c9fabc8d7c8d1e55aff167ce90d49926ca
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2022-12-03 10:35:32 +0530

    Create add2value.c

---
+++ Task/add2value.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+void main()
+
+{
+	int A,B;
+	
+	printf("Enter the value of A:");
+	scanf("%d",&A);
+	
+	printf("Enter the value of B:");
+	scanf("%d",&B);
+	
+	printf("A + B = %d\n",A+B);
+	printf("A - B = %d\n",A-B);
+	printf("A / B = %d\n",A/B);
+	printf("A * B = %d\n",A*B);
+	printf("A % B = %d\n",A%B);
+}
