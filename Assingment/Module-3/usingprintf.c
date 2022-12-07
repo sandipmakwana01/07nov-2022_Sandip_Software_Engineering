6d8b46b7969904046557ef9d789c6727319c9c18
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2022-12-07 14:36:39 +0530

    Create usingprintf.c

---
+++ Assingment/Module-3/usingprintf.c
@@ -0,0 +1,16 @@
+/* WAP Display This Information using printf 
+   1. Your Name 
+   2. Your Birth date
+   3. Your Age 
+   4. Your Address */
+
+# include <stdio.h>
+
+void main()
+
+{
+    printf("Name : Sandip \n");
+    printf("DOB : 08/06/2003 \n");
+    printf("age : 20 \n");
+    printf("address : old bharvad nesh , near rabari chowk , Dubai \n");
+}
