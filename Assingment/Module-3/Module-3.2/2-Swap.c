5e902b8a81a0f6c5269bde99ff069b1672192d8f
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-01 07:25:20 +0530

    Update 2-Swap.c

--- Assingment/Module-3/Module-3.2/2-Swap.c
+++ Assingment/Module-3/Module-3.2/2-Swap.c
@@ -1,1 +1,19 @@
+/* WAP to swap two numbers without using third variable */
 
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	
+	printf("\n Enter a = ");
+	scanf("%d",&a);
+	
+	printf("\n Enter b = ");
+	scanf("%d",&b);
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	printf("\n After swap numbers\n a = %d \n b = %d ",a,b);
+}
