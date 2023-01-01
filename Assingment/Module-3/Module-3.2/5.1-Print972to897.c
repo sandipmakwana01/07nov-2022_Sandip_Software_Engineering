2e2e668119df3efbee34dfaacd009b28c502087c
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-01 07:57:01 +0530

    Create 5.1-Print972to897.c

---
+++ Assingment/Module-3/Module-3.2/5.1-Print972to897.c
@@ -0,0 +1,11 @@
+/* Looping programs: WAP to print 972 to 897 using for loop */
+
+#include<stdio.h>
+void main()
+{
+    int i;
+     for(i=972;i>=897;i--)
+     {
+         printf("\n%d",i);
+     }
+}
