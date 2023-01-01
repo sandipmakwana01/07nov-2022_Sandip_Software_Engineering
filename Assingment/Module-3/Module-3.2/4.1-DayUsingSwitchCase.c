73e3bb11203c5d391cb2750f0665d50549db84c7
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-01 07:43:47 +0530

    Create 4.1-DayUsingSwitchCase.c

---
+++ Assingment/Module-3/Module-3.2/4.1-DayUsingSwitchCase.c
@@ -0,0 +1,36 @@
+/* WAP to show Monday to Sunday using switch case */
+
+#include<stdio.h>
+void main()
+{
+	int day;
+	
+	printf("\n Please Enter Number of day = ");
+	scanf("%d",&day);
+	
+	switch(day)
+	{
+	
+		case 1:
+			printf("Monday");
+			break;
+		case 2:
+			printf("Tuesday");
+			break;
+		case 3:
+			printf("Wednesday");
+			break;
+		case 4:
+			printf("Thursday");
+			break;
+		case 5:
+			printf("Friday");
+			break;
+		case 6:
+			printf("Saturday");
+			break;
+		case 7:
+			printf("Sunday");
+			break;
+}
+}
