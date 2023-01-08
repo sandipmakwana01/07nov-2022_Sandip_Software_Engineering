6e23484f3f0b4b9d1be6a8bdcab1e4f1e12d4c94
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-08 07:58:11 +0530

    Update 6-Factorial.c

--- Assingment/Module-3/Module-3.2/6-Factorial.c
+++ Assingment/Module-3/Module-3.2/6-Factorial.c
@@ -1,3 +1,22 @@
 /*
 WAP to print factorial of given number
 */
+
+#include<stdio.h>  
+
+int main()    
+{    
+    int i,num,f=1; 
+
+    printf("Enter a number: ");    
+    scanf("%d",&num); 
+       
+    for(i=1;i<=num;i++)
+    {    
+        f=f*i;    
+    }    
+
+    printf("Factorial of %d is: %d",num,f);   
+
+    return 0;  
+}  
