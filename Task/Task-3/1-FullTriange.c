4e750f56abce1bc4d96f609024a01baae38fccb9
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-08 15:28:59 +0530

    Update 1-FullTriange.c

--- Task/Task-3/1-FullTriange.c
+++ Task/Task-3/1-FullTriange.c
@@ -1,1 +1,53 @@
-1
+#include<stdio.h>
+
+int main()
+{
+  int a,b,n;
+
+  printf("\nEnter the value :");
+  scanf("%d",&n);
+
+  for(a=1;a<=n;a++)
+  {
+    printf("\n");
+
+    for(b=1;b<=(n-a);b++)
+    {
+        printf("  ");
+    }
+
+    for(b=1;b<=a;b++)
+    {
+        printf(" %d",b);
+    }
+
+    for(b=1;b<a;b++)
+    {
+        printf(" %d",(a-b));
+    }
+  }
+
+  for(a=1;a<=n;a++)
+  {
+    printf("\n");
+
+    for(b=1;b<=a;b++)
+    {
+      printf("  ");
+    }
+
+    for(b=1;b<(n-a);b++)
+    {
+       printf(" %d",b);
+    }
+
+    for(b=(n-a);b>0;b--)
+    {
+       printf(" %d",b);
+    }
+
+  }
+
+  return 0;
+  
+}
