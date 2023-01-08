11c78dc6570589ab2c0d3065a6cd3a18bc4737b0
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-08 13:20:52 +0530

    Update 12.1-Pattern.c

--- Assingment/Module-3/Module-3.2/12.1-Pattern.c
+++ Assingment/Module-3/Module-3.2/12.1-Pattern.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main() 
+{
+	int i,a,n;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+	for (i=1;i<=n;i++) 
+    {
+		for (a=1;a<=i;a++) 
+        {
+			printf("%d",a%2);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
