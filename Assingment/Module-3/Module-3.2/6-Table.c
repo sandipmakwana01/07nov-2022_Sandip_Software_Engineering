443993e31ca8685f9cde15345637ce798ae9ef4e
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-08 07:48:23 +0530

    Update 6-Table.c

--- Assingment/Module-3/Module-3.2/6-Table.c
+++ Assingment/Module-3/Module-3.2/6-Table.c
@@ -1,1 +1,19 @@
 /* WAP to print table up to given numbers */
+
+#include <stdio.h> 
+
+int main()  
+{  
+    int num, i;  
+
+    printf (" Enter a number the tables: ");  
+    scanf (" %d", &num); 
+      
+    printf ("\n Table of %d", num);  
+    
+    for ( i = 1; i <= 10; i++)  
+    {  
+        printf ("\n %d * %d = %d", num, i, (num*i));  
+    }  
+    return 0;  
+}  
