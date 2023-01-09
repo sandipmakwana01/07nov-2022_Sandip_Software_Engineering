63782088522b713dd8b2a92381a18a782bb8627f
sandipmakwana01 <118188100+sandipmakwana01@users.noreply.github.com>
2023-01-09 08:55:26 +0530

    Create 4.1-Compare.c

---
+++ Task/Task-3/4.1-Compare.c
@@ -0,0 +1,49 @@
+/*
+Write a program in C to compare two strings without using string library functions. 
+*/
+#include <stdio.h> 
+
+int compare(char[],char[]);  
+int main()  
+
+{  
+   char str1[20];  
+   char str2[20]; 
+    
+   printf("Enter the first string : ");  
+   scanf("%s",str1);  
+
+   printf("Enter the second string : ");  
+   scanf("%s",str2);  
+
+   int c= compare(str1,str2); 
+  
+   if(c==0)  
+   printf("strings are same");  
+   else  
+   printf("strings are not same");  
+  
+    return 0;  
+}  
+  
+
+int compare(char a[],char b[])  
+{  
+    int j=0,i=0; 
+   
+    while(a[i]!='\0' &&b[i]!='\0')    
+    {  
+       if(a[i]!=b[i])  
+       {  
+           j=1;  
+           break;  
+       }  
+       i++;  
+    }  
+
+    if(j==0)  
+    return 0;  
+    else  
+    return 1;  
+
+}
